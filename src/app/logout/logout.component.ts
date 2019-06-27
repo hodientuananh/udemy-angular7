be73b4e90c85d352d9ba2acddb2baa064e3bd320
Tuan Anh Ho Dien <anhhdt1@fsoft.com>
2019-06-27 16:33:51 +0700

    log out by delete session item

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HardcodeAuthenticationService } from '../service/hardcode-authentication.service';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private hardcodeAuthenticationService: HardcodeAuthenticationService
+  ) { }
 
   ngOnInit() {
+    this.hardcodeAuthenticationService.logout();
   }
 
 }
