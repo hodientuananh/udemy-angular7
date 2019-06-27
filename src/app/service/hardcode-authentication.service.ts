defa8fed26a696fb50d9555fec5b3e2e340c3b23
Tuan Anh Ho Dien <anhhdt1@fsoft.com>
2019-06-27 15:04:21 +0700

    create authentication service

--- src/app/service/hardcode-authentication.service.ts
+++ src/app/service/hardcode-authentication.service.ts
@@ -8,11 +8,17 @@ export class HardcodeAuthenticationService {
   constructor() { }
 
   authenticate(username, password){
-    if (username === "benkinmat" && password === "password"){
+    if (username === "benkinmat" && password === "password"){      
+      sessionStorage.setItem('authenticateUser', username);
       return true;
     }
     else {
       return false;
     }
+  }
+
+  isUserLoggedIn(){
+    let user = sessionStorage.getItem('authenticateUser');
+    return !(user === null);
   }
 }
