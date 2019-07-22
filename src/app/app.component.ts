47969982c5a652d0a344b5065b07dad0a140fd0f
Tuan Anh Ho Dien <benkinmat@gmail.com>
2019-07-22 23:15:19 +0700

    change todo list with style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // template: '<h1>{{title}}</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
